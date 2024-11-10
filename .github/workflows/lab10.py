f3b4f800d0f0b45b8c83c9a1d1f469cf3d45ed4e
kingchangyoon <166274679+kingchangyoon@users.noreply.github.com>
2024-11-10 22:23:08 +0900

    Add files via upload

---
+++ .github/workflows/lab10.py
@@ -0,0 +1,7 @@
+number = int(input("Enter a number: "))
+
+for i in range(1, number+1):  # 1부터 number까지 반복
+    if number % i == 0:      # number를 i로 나누었을 때 나머지가 0이면
+        print(i, end=" ")   # 약수를 공백으로 구분하여 출력
+
+print()
